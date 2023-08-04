01b0c1b1bb8ab4c30f880518574811afa681b958
Gümrah Sindar <gsindar@gmail.com>
2023-08-04 15:23:44 +0300

    define variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,34 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  // darkMode: 'class',
   theme: {
+    fontFamily: {
+      raleway: ['var(--font-raleway)'],
+      open_sans: ['var(--font-open-sans)'],
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        darkBlue: 'hsl(217, 28%, 15%)',
+        darkBlue1: 'hsl(218, 28%, 13%)',
+        darkBlue2: 'hsl(216, 53%, 9%)',
+        darkBlue3: ' hsl(219, 30%, 18%)',
+        accentCyan: 'hsl(176, 68%, 64%)',
+        accentBlue: 'hsl(198, 60%, 50%)',
+        lightRed: 'hsl(0, 100%, 63%)',
       },
+
+      backgroundImage: (theme) => ({
+        'logo-dark-mode': "url('/logo-dark-mode.svg')",
+        'logo-light-mode': "url('/logo-light-mode.svg')",
+        'curvy-light-mode': "url('/bg-curvy-dark-mode.svg')",
+        'curvy-dark-mode': "url('/bg-curvy-light-mode.svg')",
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      backgroundImage: ['dark'],
     },
   },
   plugins: [],
