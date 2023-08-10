b9d80aec2dc36849f0fb05b2dea6531780a72e04
Gümrah Sindar <gsindar@gmail.com>
2023-08-10 17:46:23 +0300

    complete hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
+import Hero from '@/components/Hero'
 
 export default function Home() {
-  return <main className="dark:bg-slate-400 ">Hello World</main>
+  return (
+    <main>
+      <Hero />
+    </main>
+  )
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+
+const Hero = () => {
+  return (
+    <section id="hero" className="bg-curvy-light-mode bg-contain bg-bottom bg-no-repeat dark:bg-curvy-dark-mode">
+      {/* Hero Container */}
+      <div className="container mx-auto px-6 pb-52 text-center md:pt-20">
+        <Image src="/illustration-intro.png" alt="illustration" width={534} height={720} className="mx-auto" />
+        <h1 className="mx-auto mb-10 mt-14 max-w-2xl text-3xl font-bold leading-normal md:text-4xl">
+          All your files in one secure location, accessible anywhere.
+        </h1>
+        <p className="mx-auto mb-10 max-w-sm text-sm md:max-w-xl md:text-lg">
+          Fylo stoes all your most important files in one scure location. Access them wherever you need, share and
+          collaborate with friends, famil and co-workers.
+        </p>
+        <button className="w-52 rounded-full bg-gradient-to-l from-accentBlue to-accentCyan p-3 text-sm text-white duration-200 hover:scale-95">
+          Get Started
+        </button>
+      </div>
+    </section>
+  )
+}
+export default Hero
