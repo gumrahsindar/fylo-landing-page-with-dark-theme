ac5e580dc0cf98398c8c4fc93a2cad9592b1108e
gumrahsindar <gsindar@gmail.com>
2023-08-17 18:04:27 +0300

    add smooth scrolling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Header from '@/components/Header'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Raleway, Open_Sans } from 'next/font/google'
+import Footer from '@/components/Footer'
 
 const raleway = Raleway({ subsets: ['latin'], display: 'swap', variable: '--font-raleway' })
 const open_sans = Open_Sans({ subsets: ['latin'], display: 'swap', variable: '--font-open-sans' })
@@ -13,10 +14,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${raleway.variable} ${open_sans.variable}`}>
-      <body className="font-open_sans dark:bg-darkBlue dark:text-white">
+    <html lang="en" className={`${raleway.variable} ${open_sans.variable} scroll-smooth`}>
+      <body className="font-open_sans duration-500 dark:bg-darkBlue dark:text-white">
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   )
