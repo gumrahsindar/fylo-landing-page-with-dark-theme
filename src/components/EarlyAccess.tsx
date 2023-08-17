38b8582d05a20f7b4e9ca950b53f35c9899cc7b0
gumrahsindar <gsindar@gmail.com>
2023-08-17 18:03:43 +0300

    add email validation

--- src/components/EarlyAccess.tsx
+++ src/components/EarlyAccess.tsx
@@ -12,12 +12,13 @@ const EarlyAccess = () => {
         <div className="flex flex-col items-start space-y-6 md:flex-row md:space-x-6 md:space-y-0">
           <div className="w-full md:flex-1">
             <input
-              type="text"
-              className="w-full rounded-full px-10 py-3 text-black focus:outline-none"
+              type="email"
+              className="peer w-full rounded-full px-10 py-3 text-black focus:outline-none"
               placeholder="email@example.com"
             />
+            <p className="invisible mt-2 text-sm text-red-400 peer-invalid:visible">Error, please check your email</p>
           </div>
-          <button className="w-full rounded-full bg-accentCyan p-3 px-6 duration-200 hover:scale-95 md:w-56">
+          <button className="w-full rounded-full bg-accentCyan bg-gradient-to-l from-accentBlue to-accentCyan p-3 px-6 text-white duration-200 hover:scale-95 md:w-56">
             Get Started For Free
           </button>
         </div>
